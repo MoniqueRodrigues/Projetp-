45cd49321d082de3c33f71429c8c95fdf51dda8b
Monique Rodrigues <moniqueoliveira0104@gmail.com>
2018-08-17 22:54:14 -0300

    Incluso css e js no quiz

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-function clique(sim){
-    return sim;
+let gabarito=["a","a","a","b"];
+let respostaUsuario=[];
 
+
+
+//OBTER BOTAO DO HTML PARA PARA O JS.
+let botoes = document.querySelectorAll(".btn")
+//console.log(botoes)
+
+/*
+for(i = 0; i < botoes.length; i++) {
+    botoes[i].addEventListener('click', buttonClick)
+}
+*/
+
+//CRIAÇÃO DE LOOPING ATRAVÉS DO FOREACH.
+//ADICIONAR EVENTO NO BOTÃO.
+botoes.forEach(function(botao){
+    botao.addEventListener('click', buttonClick)
+})
+
+/*botao.addEventListener('click', function() {
+    console.log('cliquei');
+});
+*/
+
+
+function buttonClick() {
+    console.log('cliquei')
 }
